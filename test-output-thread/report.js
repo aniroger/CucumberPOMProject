d21d4e99a5daab28f9c0339464b39d7d3ed193c9
aniroger <anirudhsharma303@gmail.com>
2023-01-14 17:00:45 +0530

    Second Commit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,36 +1,80 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "d3401237-5d02-4b9a-b759-870ad9916e1f",
-    "feature": "Login Page Feature",
-    "scenario": "Forgot Password Link",
-    "start": 1673543382183,
+    "id": "8a684527-24f4-466c-a354-d88b2d30f18f",
+    "feature": "Login Search Page Feature",
+    "scenario": "Login Search Data",
+    "start": 1673693677400,
     "group": 1,
     "content": "",
-    "tags": "@login,@smoke,@regression,",
-    "end": 1673543383619,
-    "className": "skipped"
-  },
-  {
-    "id": "1f824f7d-203b-4c1d-9be7-0a36f0289577",
-    "feature": "Login Page Feature",
-    "scenario": "Login Page Title",
-    "start": 1673543378309,
-    "group": 1,
-    "content": "",
-    "tags": "@login,@smoke,",
-    "end": 1673543382169,
+    "tags": "",
+    "end": 1673693686294,
     "className": "passed"
   },
   {
-    "id": "12d158aa-1ac1-4777-8aff-8af24f6b3b16",
+    "id": "2321eda7-065d-4352-8866-94c21712c1f7",
     "feature": "Login Page Feature",
-    "scenario": "Login with Correct credentials",
-    "start": 1673543383677,
+    "scenario": "Login Page Title",
+    "start": 1673693686297,
     "group": 1,
     "content": "",
     "tags": "@login,@smoke,",
-    "end": 1673543389914,
+    "end": 1673693689200,
+    "className": "passed"
+  },
+  {
+    "id": "137f4b50-51e2-4cbb-8e64-17eb85fdec12",
+    "feature": "Home Page feature",
+    "scenario": "Home Improvement Count",
+    "start": 1673693651464,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1673693662785,
+    "className": "failed"
+  },
+  {
+    "id": "7b2c2071-b922-4e8c-91c5-b3634e03bc7c",
+    "feature": "Login Page Feature",
+    "scenario": "Forgot Password Link",
+    "start": 1673693689203,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@smoke,@regression,",
+    "end": 1673693690716,
+    "className": "skipped"
+  },
+  {
+    "id": "ec31475e-2dd6-4d45-9bfe-4596f4056ee6",
+    "feature": "Login Search Page Feature",
+    "scenario": "Login Search Data",
+    "start": 1673693662795,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1673693677396,
+    "className": "passed"
+  },
+  {
+    "id": "faad5d47-cc64-48a0-86ba-9c006d7bfeb1",
+    "feature": "Login Page Feature",
+    "scenario": "Login with Correct credentials",
+    "start": 1673693690723,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@smoke,",
+    "end": 1673693695403,
+    "className": "passed"
+  },
+  {
+    "id": "fd65888e-3035-4fef-a6e3-8ca94ee4d036",
+    "feature": "Home Page feature",
+    "scenario": "Title of your scenario",
+    "start": 1673693642068,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1673693651457,
     "className": "passed"
   }
 ]);
